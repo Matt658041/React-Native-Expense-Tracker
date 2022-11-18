5e3d583089e72cc79ffe959d768f262179d52fa5
Matt658041 <91630948+Matt658041@users.noreply.github.com>
2022-11-18 10:08:09 -0600

    setting up dummy data as a placeholder for context that will be applied later

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import ExpensesOutput from '../components/Expenses Output/ExpensesOutput'
 
 export default function AllExpenses() {
   return (
     <View>
-      <Text>AllExpenses</Text>
+     <ExpensesOutput expensesPeriod="Total"/>
     </View>
   )
 }
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,10 +1,11 @@
-import { View, Text } from 'react-native'
+import { View } from 'react-native'
 import React from 'react'
+import ExpensesOutput from '../components/Expenses Output/ExpensesOutput'
 
 export default function RecentExpenses() {
   return (
     <View>
-      <Text>RecentExpenses</Text>
+      <ExpensesOutput expensesPeriod="Last 7 days"/>
     </View>
   )
 }
